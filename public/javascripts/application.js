ab66f8fd5258bed2003aa5d312df1fce82124f53
Rohit Rox <link2j.roy@gmail.com>
2012-08-23 09:56:27 +0545

    files updated according to faye server url

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://fayeredis.herokuapp.com/faye');
+  var faye = new Faye.Client('http://myfaye.herokuapp.com/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
